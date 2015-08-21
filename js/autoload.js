80ccb05eb780ae2a449536f2fec2cd02f76ca4ac
njetoperz <helpme@njetoperz.com>
2015-08-21 10:44:23 +0200

    contact form modification
    TODO: complete ajax submit

--- js/autoload.js
+++ js/autoload.js
@@ -9,4 +9,25 @@ $(document).ready(function() {
     	stopOnHover : true
     });
 
+    $("#contact-form").submit(function(e){
+    	e.preventDefault();
+  	});
+
+    $( '#contact-form-submit' ).on( 'submit', function(e) {
+	    e.preventDefault();
+	    alert("sending email");
+	    var email = $('#InputEmail').val();
+		var name = $('#InputName').val();
+		var telephone = $('#InputTelephone').val();
+		var postalCode = $('#InputPostalCode').val();
+		alert(email+' '+name+' '+telephone+' '+postalCode);
+	    $.ajax({
+	        type: "POST",
+	        url: host+'/mail/',
+	        data: {email:email, name:name, telephone:telephone, postalCode: postalCode}
+	        success: function() {
+	        alert( "ok" );
+	        }
+	    });
+	});
 });
